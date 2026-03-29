9545c2ac064dbc06f5dfd1a463d64aeb000cd3ad
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2026-03-29 17:28:45 +0500

    java26

---
+++ src/main/java/uz/brb/java26/Java26Application.java
@@ -0,0 +1,13 @@
+package uz.brb.java26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java26Application.class, args);
+    }
+
+}
---
+++ src/test/java/uz/brb/java26/Java26ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.brb.java26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
